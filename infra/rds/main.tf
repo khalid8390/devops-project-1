f7bb5b6adbbab2bd85a4f1be47bf30a9ea9dfe73
khalid8390 <139584495+khalid8390@users.noreply.github.com>
2025-06-24 13:03:06 +0530

    Update main.tf

--- infra/rds/main.tf
+++ infra/rds/main.tf
@@ -16,7 +16,7 @@ resource "aws_db_instance" "default" {
   allocated_storage       = 10
   storage_type            = "gp2"
   engine                  = "mysql"
-  instance_class          = "db.t2.micro"
+  instance_class          = "db.t3.micro"
   identifier              = var.mysql_db_identifier
   username                = var.mysql_username
   password                = var.mysql_password
